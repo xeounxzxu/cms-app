314ed495d5e58a55e2574c7584c78f9f93ff6fdf
‘gonzo’ <pnw123456@icloud.com>
2020-10-22 23:57:38 +0900

    SystemDashboard Setting

---
+++ webapp/src/assests/modules/containers/system/Cateogry.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default () =>{
+    return (
+       <div>
+          <span>category</span>
+       </div>
+    )
+}
---
+++ webapp/src/assests/modules/containers/system/User.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () =>{
+    return(
+        <div>
+            <span>User</span>
+        </div>
+    )
+}
