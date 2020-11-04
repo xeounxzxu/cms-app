1e92299aa5bf3b1c2a5674af1c441444d4386ce8
‘gonzo’ <pnw123456@icloud.com>
2020-11-04 23:15:53 +0900

    React Fix Main Container

---
+++ webapp/src/assests/modules/main/main.reducer.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import {FAILURE, REQUEST, SUCCESS} from "../../utils/action-type.util";
+
+const ACTION_TYPE = {
+  FETCH_MENU_DATA : "main/FETCH_MENU_DATA"
+}
+const initState = {
+   load : false ,
+   error : null,
+   menuData : [],
+}
+
+export default (state = initState , action) =>{
+   switch (action.type) {
+      case SUCCESS(ACTION_TYPE.FETCH_MENU_DATA):
+         return {
+            ...state,
+            menuData: action.payload,
+            load: false,
+            error : null
+         }
+      case REQUEST(ACTION_TYPE.FETCH_MENU_DATA):
+         return {
+            ...state,
+            load: true,
+            error: null
+         }
+      case FAILURE(ACTION_TYPE.FETCH_MENU_DATA):
+         return {
+            ...state,
+            load : false,
+            error: null
+         }
+      default:
+         return state
+   }
+}
+
+export const getToMenu = () => async (dispatch, getState) => {
+   await dispatch({
+      type: ACTION_TYPE.FETCH_MENU_DATA,
+      payload: axios.get('/common/menu')
+   })
+}
